a358211409bec552c51c89635132c19daf08b054
Mauricio Stampella <linkinmjs@gmail.com>
2019-09-08 22:53:53 -0300

    added board to html, and logic in component

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -12,8 +12,8 @@ export class BoardComponent implements OnInit {
 
   PEERS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 
-  brick: brick[][] = []
-
+  bricks: brick[][] = []
+  boardRevealed:boolean=false;
   remainBrick: 0;
   remainMines: 0;
   time: 0;
@@ -21,32 +21,48 @@ export class BoardComponent implements OnInit {
 
   
   constructor() {
-    for (let y = 0; y < 8; y++) {
-      this.brick[y] = []
-      for (let x = 0; x < 8; x++) {
-        this.brick[y][x] = new brick(y, x);
-        //console.log(this.brick);
-      }
-    }
-
-
-
+  
   }
 
 
 
   ngOnInit() {
     //create 5 mines
-    for (let i = 0; i < 5; i++) {
-      this.getRandomBrick().mine = true;
-    }
+    this.setupBoard();
   }
 
-  getRandomBrick(): brick {
-    const y = Math.floor(Math.random() * this.brick.length);
-    const x = Math.floor(Math.random() * this.brick[y].length);
-    //console.log(this.brick[y][x]);
-    return this.brick[y][x];
+  detectMine(i, j ){
+    console.log(this.bricks[i][j].mine)
+  }
+  revealBrick(i,j){
+    let currentBick = this.bricks[i][j];
+    if(currentBick.mine === true){
+      alert('Rompiste');
+      this.boardRevealed = true;
+    } else {
+      console.log("ni bosta")
+    }
+  }
+  setupBoard(){
+  this.bricks = []
+  this.boardRevealed=false;
+  this.remainBrick= 0;
+  this.remainMines= 0;
+  this.time= 0;
+    for (let y = 0; y < 8; y++) {
+      this.bricks[y] = []
+      for (let x = 0; x < 8; x++) {
+        this.bricks[y][x] = new brick(y, x);
+        //console.log(this.brick);
+      }
+      // console.log(this.bricks)
+    }
+    for (let i = 0; i < 5; i++) {
+      const y = Math.floor(Math.random() * this.bricks.length);
+      const x = Math.floor(Math.random() * this.bricks[y].length);
+      this.bricks[x][y].mine = true;    
+      console.log(x,y) 
+    }
   }
 
 }
